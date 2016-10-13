09e498c7d93232ba8fe9997b5dff7e950036692e
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-10-13 13:24:52 +0300

    creates and styles font-page.php

---
+++ wp-content/themes/lander/front-page.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The custom template for the one-page style page front. kicks in automatically.
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area lander-page">
+		<main id="main" class="site-main" role="main">
+
+			<section id="call-to-action">
+				<div class="indent">
+					<div class="front-left">
+						<h2 class="section-title">Call to action</h2>
+					</div>
+					<div class="front-right">
+					Form
+					</div>
+				</div>
+			</section>
+			<section id="testimonials">
+				<div class="indent">
+					Testimonials
+				</div>
+			</section>
+			<section id="services">
+				<div class="indent">
+					<h2 class="section-title">Services</h2>
+					Some Text
+					<ul>
+						<li>item 1</li>
+						<li>item 2</li>
+						<li>item 3</li>
+					</ul>
+				</div>
+			</section>
+			<section id="meet">
+				<div class="indent">
+					<h2 class="section-title">Meet Me</h2>
+				</div>
+			</section>
+			<section id="contact">
+				<div class="indent">
+					<div class="front-left">
+						<h2 class="section-title">Contact Us</h2>
+					</div>
+					<div class="front-right">
+							Address
+					</div>
+				</div>
+			</section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/lander/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* The functions file for the Lander child theme
+*/
+
+function lander_scripts(){
+	if ( is_front_page() ) {
+		wp_enqueue_style('lander-styles', get_stylesheet_directory_uri() . '/lander-style.css');
+	}
+}
+
+add_action('wp_enqueue_scripts', 'lander_scripts');
